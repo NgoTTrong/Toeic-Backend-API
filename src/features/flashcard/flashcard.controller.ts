1e439ebda8b6ebe7f70608c1b5856d06518cfbef
nguyenleminhbao <nguyenleminhbaott5@gmail.com>
2024-03-16 20:32:10 +0700

    update module flashcard

--- src/features/flashcard/flashcard.controller.ts
+++ src/features/flashcard/flashcard.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { FlashCardService } from "./flashcard.service";
 import { CreateFlashCardDto } from "./dto/create-flashcard.dto";
-import { GetUser } from "src/core/decorators";
+import { GetUser, Public } from "src/core/decorators";
 import { Payload } from 'src/core/type/jwt.payload';
 import { CreateWordDto } from "./dto/create-word.dto";
+import { UpdateFlashCardDto } from "./dto/update-flahcard.dto";
 
 
 @Controller('flashcard')
@@ -14,26 +15,35 @@ export class FlashcardController{
 
     @Post('/create')
     create(@Body() createFlashCardDto: CreateFlashCardDto, @GetUser() user: Payload){
-        return this.flashcardService.create(createFlashCardDto.title,user.id)
+        return this.flashcardService.create(createFlashCardDto,user.id)
     }
 
+    @Public()
     @Post('/create-word/:id')
-    createWord(@Body() createWordDto: CreateWordDto, @Param() id: string){
+    createWord(@Body() createWordDto: CreateWordDto, @Param('id') id: string){
         return this.flashcardService.createWord(id,createWordDto)
     }
 
+    
     @Get()
     findAll(@GetUser() user: Payload){
         return this.flashcardService.findAll(user.id);
     }
 
+    @Public()
+    @Put(':id')
+    updateFlashcard(@Param('id') id: string, @Body() updateFlashCardDto: UpdateFlashCardDto){
+        return this.flashcardService.update(id,updateFlashCardDto)
+    }
+
+    @Public()
     @Get(':id')
-    findOne(@Param() id: string, @GetUser() user: Payload){
-        return this.flashcardService.findOne(id,user.id)
+    findOne(@Param('id') id: string){
+        return this.flashcardService.findOne(id)
     }
 
     @Delete(':id')
-    remove(@Param() id: string){
+    remove(@Param('id') id: string){
         return this.flashcardService.remove(id)
     }
 
