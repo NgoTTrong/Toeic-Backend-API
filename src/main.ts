40b428fb2d083f0a8ba94533190636cc12987db0
Aura Mac device <auramac@Mac-mini-cua-Aura.local>
2024-03-26 11:56:51 +0700

    Fix

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import { environments } from './environments';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //use for testing only, we will remove later.
-  app.enableCors({
-    origin: 'http://localhost:3000',
-  });
+  app.enableCors();
 
   await app.listen(environments.port || 3001, () =>
     console.log(`Server is running in port ${environments.port || 3001}`),
